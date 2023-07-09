2fb47a497e8c7fc32ad560339eabd1677187bb6d
Thilina Ratnayaka <thilinarmtb@gmail.com>
2023-07-08 21:04:59 -0500

    Update bp5.sh

--- CEED/BP5/bp5.sh
+++ CEED/BP5/bp5.sh
@@ -2,6 +2,10 @@
 
 : ${BP5_INSTALL_DIR:=`pwd`/install}
 : ${BP5_CC:=cc}
+: ${BP5_OPENCL:=OFF}
+: ${BP5_CUDA:=OFF}
+: ${BP5_HIP:=OFF}
+: ${BP5_NOMP:=OFF}
 
 ### Don't touch anything that follows this line. ###
 BP5_CURRENT_DIR=`pwd`
@@ -10,6 +14,11 @@ BP5_BUILD_DIR=${BP5_CURRENT_DIR}/build
 mkdir -p ${BP5_BUILD_DIR} 2> /dev/null
 
 cmake -DCMAKE_C_COMPILER=${BP5_CC} -DCMAKE_INSTALL_PREFIX=${BP5_INSTALL_DIR} \
-  -B ${BP5_BUILD_DIR} -S ${BP5_CURRENT_DIR}
+  -B ${BP5_BUILD_DIR} \
+  -S ${BP5_CURRENT_DIR} \
+  -DENABLE_OPENCL=${BP5_OPENCL} \
+  -DENABLE_CUDA=${BP5_CUDA} \
+  -DENABLE_HIP=${BP5_HIP} \
+  -DENABLE_NOMP=${BP5_NOMP}
   
 cmake --build ${BP5_BUILD_DIR} --target install -j10
