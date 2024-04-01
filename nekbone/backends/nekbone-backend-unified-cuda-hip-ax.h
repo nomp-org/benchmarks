af1df1b6baecc9e124c72a5f9c8ba990da09044d
Thilina Ratnayaka <thilinarmtb@gmail.com>
2024-04-01 01:12:28 -0400

    Use NEKBONE_IDX4 to cleanup ax code

--- nekbone/backends/nekbone-backend-unified-cuda-hip-ax.h
+++ nekbone/backends/nekbone-backend-unified-cuda-hip-ax.h
@@ -5,18 +5,18 @@
 #error "NX1 must be defined !"
 #endif
 
-#define NEKBONE_IDX2(i, j)    ((i) + NX1 * (j))
-#define NEKBONE_IDX3(i, j, k) ((i) + NX1 * ((j) + NX1 * (k)))
+#define NEKBONE_IDX2(i, j)       ((i) + NX1 * (j))
+#define NEKBONE_IDX4(i, j, k, e) ((i) + NX1 * ((j) + NX1 * ((k) + NX1 * (e))))
 
 #define ax_kernel TOKEN_PASTE(ax_kernel_v00_, NX1)
 
 __global__ static void __launch_bounds__(NX1 *NX1 *NX1)
     ax_kernel(scalar *__restrict__ w, const scalar *__restrict__ u,
               const scalar *__restrict__ G, const scalar *__restrict__ D) {
-  const uint ebase = blockIdx.x * NX1 * NX1 * NX1;
-  const uint i     = threadIdx.x;
-  const uint j     = threadIdx.y;
-  const uint k     = threadIdx.z;
+  const uint e = blockIdx.x;
+  const uint i = threadIdx.x;
+  const uint j = threadIdx.y;
+  const uint k = threadIdx.z;
 
   __shared__ scalar s_D[NX1][NX1];
   __shared__ scalar s_ur[NX1][NX1][NX1];
@@ -30,13 +30,13 @@ __global__ static void __launch_bounds__(NX1 *NX1 *NX1)
   __syncthreads();
 
   for (uint l = 0; l < NX1; ++l) {
-    s_ur[k][j][i] += s_D[i][l] * u[ebase + NEKBONE_IDX3(l, j, k)];
-    s_us[k][j][i] += s_D[j][l] * u[ebase + NEKBONE_IDX3(i, l, k)];
-    s_ut[k][j][i] += s_D[k][l] * u[ebase + NEKBONE_IDX3(i, j, l)];
+    s_ur[k][j][i] += s_D[i][l] * u[NEKBONE_IDX4(l, j, k, e)];
+    s_us[k][j][i] += s_D[j][l] * u[NEKBONE_IDX4(i, l, k, e)];
+    s_ut[k][j][i] += s_D[k][l] * u[NEKBONE_IDX4(i, j, l, e)];
   }
   __syncthreads();
 
-  const uint gbase = 6 * (ebase + NEKBONE_IDX3(i, j, k));
+  const uint gbase = 6 * NEKBONE_IDX4(i, j, k, e);
   scalar     r_G00 = G[gbase + 0];
   scalar     r_G01 = G[gbase + 1];
   scalar     r_G02 = G[gbase + 2];
@@ -62,13 +62,13 @@ __global__ static void __launch_bounds__(NX1 *NX1 *NX1)
     wo += s_D[l][i] * s_ur[k][j][l] + s_D[l][j] * s_us[k][l][i] +
           s_D[l][k] * s_ut[l][j][i];
   }
-  w[ebase + NEKBONE_IDX3(i, j, k)] = wo;
+  w[NEKBONE_IDX4(i, j, k, e)] = wo;
 }
 
 #undef ax_kernel
 
 #undef NEKBONE_IDX2
-#undef NEKBONE_IDX3
+#undef NEKBONE_IDX4
 
 #undef TOKEN_PASTE
 #undef TOKEN_PASTE_
