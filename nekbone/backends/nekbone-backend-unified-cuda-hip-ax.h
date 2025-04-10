f3238523bb8abbd0f9d3e8be87c20980d7a8c36f
Thilina Ratnayaka <thilinarmtb@gmail.com>
2025-04-10 17:18:03 +0000

    Minor refactor in unified-cuda-hip-ax

--- nekbone/backends/nekbone-backend-unified-cuda-hip-ax.h
+++ nekbone/backends/nekbone-backend-unified-cuda-hip-ax.h
@@ -5,8 +5,8 @@
 #error "NX1 must be defined !"
 #endif
 
-#define NEKBONE_IDX2(i, j)       ((i) + NX1 * (j))
-#define NEKBONE_IDX4(i, j, k, e) ((i) + NX1 * ((j) + NX1 * ((k) + NX1 * (e))))
+#define IDX2(i, j)       ((i) + NX1 * (j))
+#define IDX4(i, j, k, e) ((i) + NX1 * ((j) + NX1 * ((k) + NX1 * (e))))
 
 #define ax_kernel TOKEN_PASTE(ax_kernel_v00_, NX1)
 
@@ -24,19 +24,19 @@ __global__ static void __launch_bounds__(NX1 *NX1 *NX1)
   __shared__ scalar s_ut[NX1][NX1][NX1];
 
   s_ur[k][j][i] = 0;
-  if (k == 0) s_D[j][i] = D[NEKBONE_IDX2(i, j)];
+  if (k == 0) s_D[j][i] = D[IDX2(i, j)];
   s_us[k][j][i] = 0;
   s_ut[k][j][i] = 0;
   __syncthreads();
 
   for (uint l = 0; l < NX1; ++l) {
-    s_ur[k][j][i] += s_D[i][l] * u[NEKBONE_IDX4(l, j, k, e)];
-    s_us[k][j][i] += s_D[j][l] * u[NEKBONE_IDX4(i, l, k, e)];
-    s_ut[k][j][i] += s_D[k][l] * u[NEKBONE_IDX4(i, j, l, e)];
+    s_ur[k][j][i] += s_D[i][l] * u[IDX4(l, j, k, e)];
+    s_us[k][j][i] += s_D[j][l] * u[IDX4(i, l, k, e)];
+    s_ut[k][j][i] += s_D[k][l] * u[IDX4(i, j, l, e)];
   }
   __syncthreads();
 
-  const uint gbase = 6 * NEKBONE_IDX4(i, j, k, e);
+  const uint gbase = 6 * IDX4(i, j, k, e);
   scalar     r_G00 = G[gbase + 0];
   scalar     r_G01 = G[gbase + 1];
   scalar     r_G02 = G[gbase + 2];
@@ -62,13 +62,15 @@ __global__ static void __launch_bounds__(NX1 *NX1 *NX1)
     wo += s_D[l][i] * s_ur[k][j][l] + s_D[l][j] * s_us[k][l][i] +
           s_D[l][k] * s_ut[l][j][i];
   }
-  w[NEKBONE_IDX4(i, j, k, e)] = wo;
+  w[IDX4(i, j, k, e)] = wo;
 }
 
 #undef ax_kernel
 
-#undef NEKBONE_IDX2
-#undef NEKBONE_IDX4
+#undef NX1
+
+#undef IDX2
+#undef IDX4
 
 #undef TOKEN_PASTE
 #undef TOKEN_PASTE_
